db5de53ba339376c1b0ba7c6161f005c44292593
shabroek <sander1984@hotmail.com>
2015-04-04 14:37:18 +0200

    uuntracked files added

---
+++ FootballCoach/FootballCoach.Shared/Model/Match.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballCoach.Model
+{
+    public class Match
+    {
+        public DateTime Date { get; set; }
+        public Team Opponent { get; set; }
+        public IEnumerable<IMatchEvent> Events { get; set; }
+        public bool IsHomeMatch { get; set; }
+    }
+}
---
+++ FootballCoach/FootballCoach.WindowsPhone/Converters/HomeOrAwayConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace FootballCoach.Converters
+{
+    public class HomeOrAwayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is bool)) return new SolidColorBrush(Colors.Transparent);
+            if ((bool)value)
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            return new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
